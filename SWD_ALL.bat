4c8fb3636b2963063bc781aabe8394d2215842f6
allu <a.lukyanchenko@querix.com.ua>
2016-07-07 10:46:21 +0300

    update firefox

--- SWD_ALL.bat
+++ SWD_ALL.bat
@@ -3,6 +3,6 @@
 start /MIN C:\qat\chromedriver.exe
 start /MIN C:\qat\IEDriverServer.exe
 
-java -jar C:\qat\selenium-server-standalone-2.53.0.jar
+java -jar C:\qat\selenium-server-standalone-2.53.1.jar
 
 pause
