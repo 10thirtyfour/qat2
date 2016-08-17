9a054cfdc6ab15c42aa3f12081fd0db30bac1f48
allu <a.lukyanchenko@querix.com.ua>
2016-08-17 12:36:04 +0300

    update webDrivers

--- SWD_ALL.bat
+++ SWD_ALL.bat
@@ -3,6 +3,8 @@
 start /MIN C:\qat\chromedriver.exe
 start /MIN C:\qat\IEDriverServer.exe
 
-java -jar C:\qat\selenium-server-standalone-2.53.1.jar
+java -jar C:\qat\selenium-server-standalone-3.0.0.jar
+
+rem java -jar -Dwebdriver.gecko.driver=geckodriver.exe selenium-server-standalone-3.0.0.jar
 
 pause
