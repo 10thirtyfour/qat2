3e1d521a853d93cfdd10f6cde77911e0414ffc77
allu <a.lukyanchenko@querix.com.ua>
2017-04-10 16:08:39 +0300

    test commit

--- test.js
+++ test.js
@@ -5,23 +5,26 @@ var arr = "2d,1d,1d,1h,1h,1h,1h,1h,1d,2d,5d,5d,15d,10d,10d,3d,2d,1d,1d,1d,1d,1d,
 var arr = arr.split(",")
 var cont = 0;
 for (var i = 0; i < arr.length; i++)
-{
-if (arr[i].indexOf("d")>-1)
-{
+  {
+    if (arr[i].indexOf("d")>-1) {
+
 var d = parseInt(arr[i].substring(0,arr[i].indexOf("d")));
-if (d > 30) {cont+=d}else{cont+=d*8};
-if (arr[i].indexOf("h")>-1)
-{
-cont+=parseInt(arr[i].substring(arr[i].indexOf("d")+2,arr[i].indexOf("h")));
+
+if (d > 30) {cont+=d} else {cont+=d*8};
+
+if (arr[i].indexOf("h")>-1) {
+    cont+=parseInt(arr[i].substring(arr[i].indexOf("d")+2,arr[i].indexOf("h")));
+  }
 }
-}
-else 
-{
-if (arr[i].indexOf("h")>-1)
-{
-cont+=parseInt(arr[i].substring(0,arr[i].indexOf("h")));
-}
-}
-}
+else
+
+  {
+    if (arr[i].indexOf("h")>-1) {
+      cont+=parseInt(arr[i].substring(0,arr[i].indexOf("h")));
+      }
+    }
+  }
+
 console.log(cont);
+
 console.log(i);
